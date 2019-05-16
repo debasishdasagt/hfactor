0ccbf3fc8bff390c35dc52f74fa8cb397074fa53
debasishdasagt <debasishdasagt@users.noreply.github.com>
2019-05-16 19:55:41 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>H-Factor</title>
+  
+  
+  <link rel='stylesheet' href='bootstrap/bootstrap-3.3.7-dist/css/bootstrap.min.css'>
+  <link rel='stylesheet' href='css/page_style.css'>
+
+     
+
+      <style>
+         
+</style>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-default">
+  <div class="container-fluid" id="navcontainer">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">H-Factor</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Appointment<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Doctor</a></li>
+            <li><a href="#">Diagnostics</a></li>
+            <li><a href="#">Nursing Service</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Sample Collection</a></li>
+            <li><a href="#">Diagnostics Report Delivery</a></li>
+          </ul>
+        </li>
+        <li><a href="#">About US</a></li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><i class="glyphicon glyphicon-log-in"></i> Login</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+        <img src="images/img1.jpg" alt="..." class="img-responsive">
+      <div class="carousel-caption">
+         <h2 class="hidden-xs">Best Diagnostic service assured </h2><a href="#" class="btn btn-danger btn-lg " role="button"><i class="glyphicon glyphicon-phone-alt"></i> Book Now</a> 
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/img2.jpg" alt="..." class="img-responsive">
+      <!-- <a href="https://www.freepik.com/free-photos-vectors/background">Background photo created by pressfoto - www.freepik.com</a> -->
+      <div class="carousel-caption">
+         <h2 class="hidden-xs">We have listed best doctors only for you and your loved ones </h2><h4 class="visible-xs">We have listed best doctors only for you and your loved ones </h4><a href="#" class="btn btn-danger btn-lg " role="button"><i class="glyphicon glyphicon-phone-alt"></i> Book Now</a> 
+      </div>
+    </div>
+    
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    <script src='js/jquery-3.2.1.min.js'></script>
+    <script src='bootstrap/bootstrap-3.3.7-dist/js/bootstrap.min.js'></script>
+
+  
+
+</body>
+
+</html>
---
+++ index2.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="EN">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width= device-width, initial-scale = 1">
+        <title></title>
+        
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+        <style>
+            .jumbotron
+            {
+                background-color: #2e6da4;
+                color: #ffffff;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="page-header">
+                <h1>Test Bootstrap </h1>
+            </div>
+            <div class="jumbotron">
+                <div class="row">
+                    
+                    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                        <h3>Column 1</h3>
+                        here are plenty of storage drives available for different usage, but primarily users 
+                        get confused which one to choose. The difference lies in the engineering of the drive, 
+                        so while choosing any you need to understand your usage. Surveillance is one of the segment
+                        where most of the time normal hard drives being used. However there are surveillance drives 
+                        available with a little price difference, but performance is far better.
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                         <h3>Column 2</h3>
+                        here are plenty of storage drives available for different usage, but primarily users 
+                        get confused which one to choose. The difference lies in the engineering of the drive, 
+                        so while choosing any you need to understand your usage. Surveillance is one of the segment
+                        where most of the time normal hard drives being 
+                    </div>
+                    <div class="clearfix visible-sm"></div>
+                    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                         <h3>Column 3</h3>
+                        here are plenty of storage drives available for different usage, but primarily users 
+                        get confused which one to choose. The difference lies in the engineering of the drive, 
+                        so while choosing any you need to understand your usage. Surveillance is one of the segment
+                        where most of the time normal hard drives being used. However there are surveillance drives 
+                        available with a little price difference, but performance is far better.
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                         <h3>Column 4</h3>
+                        here are plenty of storage drives available for different usage, but primarily users 
+                        get confused which one to  segment
+                        where most of the time normal hard drives being used. However there are surveillance drives 
+                        available with a little price difference, but performance is far better.
+                    </div>
+                    
+                    
+                    
+                </div>
+                <p> kjesghklsdhfgksdfhgksjg kjsdfg skldhg klsjdg kjsdgh ksdhfg ksdhg kjsdhf gkjsdh gkjsdg kjsdjgh ksjdgh skdjgh 
+                    <a class="btn btn-default btn-sm" role="button" disabled="disabled"> More Info</a></p>
+                <p>
+                <div class="btn-group btn-sm" role="group">
+                    <button class="btn btn-default" role="button">Test 1</button>
+                    <button class="btn btn-default" role="button">Test 2</button>
+                    <button class="btn btn-default" role="button">Test 3</button>
+                </div>
+                </p>
+
+
+
+
+
+
+            </div>
+            
+            <div class="row">
+                
+                <div class="col-md-6 col-md-offset-6 col-lg-4 col-lg-offset-8">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <td colspan="3">Test 1</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th class="col-md-2 col-lg-2 col-sm-2">Test 1</th>
+                            <th >Test 1</th>
+                            <th>Test 1</th>
+                        </tr><tr>
+                            <td class="text-center">Test 1</td>
+                            <td class="text-center">Test 1</td>
+                            <td class="text-center">Test 1</td>
+                        </tr>
+                        <tr>
+                            <td>Test 1</td>
+                            <td>Test 1</td>
+                            <td>Test 1</td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+                
+                
+                </div></div>
+            
+            
+            
+            
+            
+        </div>
+        
+        <?php
+        // put your code here
+        ?>
+        <script src="js/jquery-3.4.1.min.js"> </script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
